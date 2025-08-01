fecbe2b07b7993eea7523d1eae40c222bb078b1d
agungSN12 <149946374+agungSN12@users.noreply.github.com>
2025-08-01 18:05:05 +0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const config = require("./src/config/config");
-
-const PORT = config.server.port || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Server on Running ${config.server.baseUrl}`);
-});
